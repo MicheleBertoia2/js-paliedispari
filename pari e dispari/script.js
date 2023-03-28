556bf4feab4fd00a85b4e41990ab1ce4f8e94f1f
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-28 17:42:16 +0200

    [add] scelta del pari o dispari

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -8,9 +8,32 @@
 const sceltaUtente = 'pari' //prompt('scegli pari o dispari');
 const numeroUtente = 5 //parseInt(prompt('inserisci un numero da 1 a 5'));
 
+const numComputer = generaNumero();
+
+const numSomma = numeroUtente + numComputer; 
+let sceltaPari = null;
+if (sceltaUtente === 'pari') {
+  sceltaPari = true
+} else if (sceltaUtente === 'dispari'){
+  sceltaPari = false;
+}
+
+
+
+
+
 function generaNumero() {
   const numeroRandom = Math.floor(Math.random() * 5 + 1);
   return numeroRandom;  
 }
 
-console.log(generaNumero());
+function controlloPari (numero1){
+  let isPari = null;
+  if (!(numero1 % 2)) {
+    isPari = true
+  }
+  return isPari;
+}
+
+
+
